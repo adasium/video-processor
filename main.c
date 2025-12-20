5fe93c9a31157071cdbf059aa404e8d0e19d20c6
adasium <adasium@proton.me>
2025-12-20 15:09:51 +0100

    working prototype

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 
+#define NOB_IMPLEMENTATION
+#include "./thirdparty/nob.h"
+
 #define MAX_FILEPATH_RECORDED   4096
 #define MAX_FILEPATH_SIZE       2048
 
@@ -17,7 +20,9 @@ void draw_slider(int x, int y, int width, float percentage) {
 
 typedef enum {
     NOTHING,
+    BACKGROUND,
     CRF,
+    SUBMIT_BTN,
 } InteractingWith;
 
 
@@ -29,6 +34,30 @@ float clampf(float value, float min, float max) {
         return max;
     }
     return value;
+}
+
+
+int run_ffmpeg(char* path, int crf) {
+
+    if(!strlen(path)) {
+        printf("[INFO] no file is selected.\n");
+        return 1;
+    }
+
+    printf("[INFO] selected file: %s\n", path);
+
+    char crf_str[3] = {
+        crf % 10 + '0',
+        crf / 10 + '0',
+    };
+
+    Nob_Cmd cmd = {0};
+    nob_cmd_append(&cmd, "ffmpeg");
+    nob_cmd_append(&cmd, "-i", path);
+    nob_cmd_append(&cmd, "-crf", crf_str);
+    nob_cmd_append(&cmd, "output.mp4");
+    if (!nob_cmd_run(&cmd)) return 1;
+    return 0;
 }
 
 
@@ -73,14 +102,27 @@ int main(void)
             10,
             10
         };
+
+
+        Vector2 mouse = GetMousePosition();
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-            Vector2 mouse = GetMousePosition();
             printf("%f %f %f %f | %f %f\n", slider_btn.x, slider_btn.y, slider_btn.width, slider_btn.height, mouse.x, mouse.y);
 
-            if (interacting_with == NOTHING) {
-                if(CheckCollisionPointRec(mouse, slider_btn)) {
-                    interacting_with = CRF;
+            while (1) {
+                if (interacting_with == NOTHING) {
+                    if(CheckCollisionPointRec(mouse, slider_btn)) {
+                        interacting_with = CRF;
+                        break;
+                    }
+
+                    if(CheckCollisionPointRec(mouse, submit_btn)) {
+                        interacting_with = SUBMIT_BTN;
+                        break;
+                    }
+                    interacting_with = BACKGROUND;
+                    break;
                 }
+                break;
             }
 
             if (interacting_with == CRF) {
@@ -92,16 +134,20 @@ int main(void)
         }
 
         if (IsMouseButtonUp(MOUSE_BUTTON_LEFT)) {
+            if(interacting_with == SUBMIT_BTN && CheckCollisionPointRec(mouse, submit_btn)) {
+                run_ffmpeg(file_path, crf);
+            }
             interacting_with = NOTHING;
         }
-
-
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawText(TextFormat("path: %s", file_path), 100, 100, 20, BLACK);
             DrawText(TextFormat("CRF: %d", crf), 100, 170, 20, BLACK);
             draw_slider(100, 200, 100, 100.f*crf/MAX_CRF);
+            if (interacting_with == SUBMIT_BTN) {
+                DrawRectangleRec(submit_btn, GRAY);
+            }
             DrawRectangleLinesEx(submit_btn, 2, BLACK);
 
             int size = MeasureText("run", 10);
